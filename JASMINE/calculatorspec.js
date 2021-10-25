e6fdecbada113252bde0fc4d893a41d603d578f3
neetukchandran <92144174+neetukchandran@users.noreply.github.com>
2021-10-25 10:32:52 +0530

    Add files via upload

---
+++ JASMINE/calculatorspec.js
@@ -0,0 +1,44 @@
+ 
+calculator = {
+  
+   currentVal:0, 
+   varAfterEachExmaple:0,
+  
+   add:function (num1) {
+      this.currentVal += num1;
+      return this.currentVal;    
+   },     
+  
+   addAny:function () {    
+      var sum = this.currentVal;
+                                
+      for(var i = 0; i < arguments.length; i++) {
+         sum += arguments[i];
+      }
+     
+      this.currentVal = sum;
+      return  this.currentVal;
+   },
+};
+
+
+describe("calculator",function() {
+  
+    //test case: 1 
+    it("Should retain the current value of all time", function () {
+       expect(calculator.currentVal).toBeDefined();
+       expect(calculator.currentVal).toEqual(0); 
+    });
+   
+    //test case: 2 
+    it("should add numbers",function() {
+       expect(calculator.add(5)).toEqual(5);
+       expect(calculator.add(5)).toEqual(10); 
+    });         
+     
+    //test case :3  
+    it("Should add any number of numbers",function () {
+       expect(calculator.addAny(1,2,3)).toEqual(6);
+    });
+ });
+ 
